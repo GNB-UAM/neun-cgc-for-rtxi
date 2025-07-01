377ab8dda5a48da6423903190a84f29673086bfd
Alicia Garrido-Peña <aliciagp96@gmail.com>
2025-07-01 12:18:57 +0200

    Hard reset

--- neun-cgc-for-rtxi.cpp
+++ neun-cgc-for-rtxi.cpp
@@ -230,6 +230,88 @@ NeunCgcForRtxi::initParameters(void)
   i_ext = 0.1;
   v0 = -65;
   diff_T = 0;
+
+
+  //TODO fix reading by YAML
+
+
+  neuron.set(Neuron::Ga, 18.82);
+  neuron.set(Neuron::Gd, 1.2);
+  neuron.set(Neuron::Ghva, 1.03);
+  neuron.set(Neuron::Glva, 0.01);
+  neuron.set(Neuron::Gnap, 0.44);
+  neuron.set(Neuron::Gnat, 1.68);
+  neuron.set(Neuron::Ia, 3.0);
+  neuron.set(Neuron::Id, 3.0);
+  neuron.set(Neuron::Ihva, 3.0);
+  neuron.set(Neuron::Ilva, 3.0);
+  neuron.set(Neuron::Inap, 3.0);
+  neuron.set(Neuron::Inat, 3.0);
+  neuron.set(Neuron::Q10_Ga, 3.0);
+  neuron.set(Neuron::Q10_Gd, 3.0);
+  neuron.set(Neuron::Q10_Ghva, 3.0);
+  neuron.set(Neuron::Q10_Glva, 3.0);
+  neuron.set(Neuron::Q10_Gnap, 3.0);
+  neuron.set(Neuron::Q10_Gnat, 3.0);
+  neuron.set(Neuron::Q10_a, 3.0);
+  neuron.set(Neuron::Q10_b, 3.0);
+  neuron.set(Neuron::Q10_e, 3.0);
+  neuron.set(Neuron::Q10_f, 3.0);
+  neuron.set(Neuron::Q10_h, 3.0);
+  neuron.set(Neuron::Q10_n, 3.0);
+  neuron.set(Neuron::Q10_r, 3.0);
+  neuron.set(Neuron::Vh_c, -41.35);
+  neuron.set(Neuron::Vh_d, -64.13);
+  neuron.set(Neuron::Vh_m, -35.2);
+  neuron.set(Neuron::Vs_c, 5.05);
+  neuron.set(Neuron::Vs_d, -4.03);
+  neuron.set(Neuron::Vs_m, 9.66);
+  neuron.set(Neuron::cm, 1.0);
+  neuron.set(Neuron::delta_a, 0.39);
+  neuron.set(Neuron::delta_b, 0.83);
+  neuron.set(Neuron::delta_e, 0.84);
+  neuron.set(Neuron::delta_f, 0.97);
+  neuron.set(Neuron::delta_h, 0.03);
+  neuron.set(Neuron::delta_n, 0.18);
+  neuron.set(Neuron::delta_r, 1.0);
+  neuron.set(Neuron::diff_T, 0.0);
+  neuron.set(Neuron::dv, 3.0);
+  neuron.set(Neuron::gamma_T, 0.05);
+  neuron.set(Neuron::tau0_a, 13.28);
+  neuron.set(Neuron::tau0_b, 226.75);
+  neuron.set(Neuron::tau0_e, 3.81);
+  neuron.set(Neuron::tau0_f, 34.68);
+  neuron.set(Neuron::tau0_h, 778.82);
+  neuron.set(Neuron::tau0_n, 14.52);
+  neuron.set(Neuron::tau0_r, 4.01);
+  neuron.set(Neuron::vca, 80.0);
+  neuron.set(Neuron::vh_a, -36.37);
+  neuron.set(Neuron::vh_b, -83.0);
+  neuron.set(Neuron::vh_e, -14.25);
+  neuron.set(Neuron::vh_f, -21.44);
+  neuron.set(Neuron::vh_h, -56.43);
+  neuron.set(Neuron::vh_n, -59.43);
+  neuron.set(Neuron::vh_r, -47.03);
+  neuron.set(Neuron::vk, -90.0);
+  neuron.set(Neuron::vna, 55.0);
+  neuron.set(Neuron::vs_a, 8.72);
+  neuron.set(Neuron::vs_b, -6.2);
+  neuron.set(Neuron::vs_e, 6.96);
+  neuron.set(Neuron::vs_f, -5.78);
+  neuron.set(Neuron::vs_h, -8.41);
+  neuron.set(Neuron::vs_n, 34.79);
+  neuron.set(Neuron::vs_r, 20.55);
+
+  neuron.set(Neuron::v, v0);
+  neuron.set(Neuron::h, 1 / (1 + exp( (neuron.get(Neuron::vh_h) - v0) / neuron.get(Neuron::vs_h))));
+  neuron.set(Neuron::r, 1 / (1 + exp( (neuron.get(Neuron::vh_r) - v0) / neuron.get(Neuron::vs_r))));
+  neuron.set(Neuron::a, 1 / (1 + exp( (neuron.get(Neuron::vh_a) - v0) / neuron.get(Neuron::vs_a))));
+  neuron.set(Neuron::b, 1 / (1 + exp( (neuron.get(Neuron::vh_b) - v0) / neuron.get(Neuron::vs_b))));
+  neuron.set(Neuron::n, 1 / (1 + exp( (neuron.get(Neuron::vh_n) - v0) / neuron.get(Neuron::vs_n))));
+  neuron.set(Neuron::e, 1 / (1 + exp( (neuron.get(Neuron::vh_e) - v0) / neuron.get(Neuron::vs_e))));
+  neuron.set(Neuron::f, 1 / (1 + exp( (neuron.get(Neuron::vh_f) - v0) / neuron.get(Neuron::vs_f))));
+  
+  
 }
 
 void
@@ -243,7 +325,6 @@ NeunCgcForRtxi::update(DefaultGUIModel::update_flags_t flag)
 
       param_names = Neuron::ParamNames();
       for (int i = 0; i < Neuron::n_parameters; i++) {
-        // vars[i+1].name = param_names[i];
     // Convert std::string to QString for setParameter
         QString paramName = QString::fromStdString(param_names[i]);
         double paramValue = static_cast<double>(
@@ -252,6 +333,7 @@ NeunCgcForRtxi::update(DefaultGUIModel::update_flags_t flag)
         setParameter(paramName, paramValue);
       }
       param_names_qt.clear();
+      param_names_qt.push_back(QString::fromStdString("Iext"));
       for (const auto& name : Neuron::ParamNames()) {
           param_names_qt.push_back(QString::fromStdString(name));
       }
@@ -259,9 +341,11 @@ NeunCgcForRtxi::update(DefaultGUIModel::update_flags_t flag)
       break;
 
     case MODIFY:
-      i_ext = getParameter("Iext").toDouble();
+      i_ext = getParameter(param_names_qt[0]).toDouble();
 
+      printf("Setting %s\n",param_names_qt[0].toStdString().c_str());
       for (int i = 0; i < Neuron::n_parameters; i++) {
+        printf("Setting %s\n",param_names_qt[i+1].toStdString().c_str());
         double value = getParameter(param_names_qt[i+1]).toDouble();
         neuron.set(static_cast<VavoulisCGCModelQ10<double>::parameter>(i), value);
     }
@@ -323,4 +407,5 @@ void
 NeunCgcForRtxi::bBttn_event(void)
 {
   printParameters();
+  printf("Input %f\n",input(0));
 }
